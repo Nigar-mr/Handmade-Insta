2283d34a84571083014b7be37ccd8e43262b1c59
Nigar-mr <nigar-muradli@mail.ru>
2019-09-11 23:32:05 +0400

    Add gunicorn

--- handmade_blog/settings.py
+++ handmade_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w&2wxk&ei5r5c$57vx%052g!3b2l1=95d&i%hggj^3nx&c04w4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
